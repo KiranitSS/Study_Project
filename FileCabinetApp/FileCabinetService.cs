1cb6fc2bbb222dda135d6e9e75f5f8822fb053ff
Lenovo2 <Lenovo2@DESKTOP-JI7ISC5>
2021-08-03 23:15:00 +0300

    Add FileCabinetServise.cs file

---
+++ FileCabinetApp/FileCabinetService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCabinetApp
+{
+    public class FileCabinetService
+    {
+        private readonly List<FileCabinetRecord> records = new List<FileCabinetRecord>();
+
+        public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            // TODO: добавьте реализацию метода
+            return 0;
+        }
+
+        public FileCabinetRecord[] GetRecords()
+        {
+            // TODO: добавьте реализацию метода
+            return Array.Empty<FileCabinetRecord>();
+        }
+
+        public int GetStart()
+        {
+            // TODO: добавьте реализацию метода
+            return 0;
+        }
+    }
+}
