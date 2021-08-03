181a25dc7c8247c5f803bb3aee654e0ba4eca509
Lenovo2 <Lenovo2@DESKTOP-JI7ISC5>
2021-08-03 17:09:38 +0300

    Add initial version of FileCabinetApp

---
+++ FileCabinetApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileCabinetApp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
