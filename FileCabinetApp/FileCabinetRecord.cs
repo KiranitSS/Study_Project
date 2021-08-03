df25cd60ab74e7ee02d50c04fa3e9a03a8057da4
Lenovo2 <Lenovo2@DESKTOP-JI7ISC5>
2021-08-03 22:59:58 +0300

    Add FileCabinetRecord.cs file

---
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCabinetApp
+{
+    public class FileCabinetRecord
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
